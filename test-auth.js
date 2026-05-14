77331236d22be7e7eb1b1dcab554afc08ee71ec1
Blackie <rob@ghost-communications.com>
2026-05-14 15:09:39 +0200

    chore: cleanup test files

--- test-auth.js
+++
@@ -1,6 +0,0 @@
-const { signInWithEmailAndPassword } = require('firebase/auth');
-try {
-  signInWithEmailAndPassword(null, 'test@test.com', 'password').catch(err => console.log('Promise rejected:', err.message));
-} catch (err) {
-  console.log('Synchronous throw:', err.message);
-}
