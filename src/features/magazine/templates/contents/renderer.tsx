58f6f14cdca472d77a6b3d8b16c6b3c6f5e9f99e
Blackie <rob@ghost-communications.com>
2026-08-12 14:17:33 +0200

    feat: link contents page articles to their page numbers

--- src/features/magazine/templates/contents/renderer.tsx
+++ src/features/magazine/templates/contents/renderer.tsx
@@ -1,6 +1,6 @@
 import type { TemplateRenderProps } from '../../domain/template-registry';
 import { PageContents } from '../shared';
 
-export default function ContentsTemplate({ viewModel, imageVersion }: TemplateRenderProps) {
-  return <PageContents data={viewModel} imageVersion={imageVersion ?? ''} />;
+export default function ContentsTemplate({ viewModel, imageVersion, pages, onNavigateToPage }: TemplateRenderProps) {
+  return <PageContents data={viewModel} imageVersion={imageVersion ?? ''} pages={pages} onNavigateToPage={onNavigateToPage} />;
 }
