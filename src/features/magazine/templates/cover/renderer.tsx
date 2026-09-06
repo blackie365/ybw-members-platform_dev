eef697eae0062df298f81594fc6f2d76c1131517
Blackie <rob@ghost-communications.com>
2026-09-06 12:42:46 +0200

    feat(magazine): numbered front-page teasers on the reader cover (#484)

--- src/features/magazine/templates/cover/renderer.tsx
+++ src/features/magazine/templates/cover/renderer.tsx
@@ -1,6 +1,13 @@
 import type { TemplateRenderProps } from '../../domain/template-registry';
 import { PageNewspaperCover } from '../shared';
 
-export default function CoverTemplate({ viewModel, imageVersion }: TemplateRenderProps) {
-  return <PageNewspaperCover data={viewModel} imageVersion={imageVersion ?? ''} />;
+export default function CoverTemplate({ viewModel, imageVersion, editionSlug, siblings }: TemplateRenderProps) {
+  return (
+    <PageNewspaperCover
+      data={viewModel}
+      imageVersion={imageVersion ?? ''}
+      editionSlug={editionSlug}
+      siblings={siblings}
+    />
+  );
 }
